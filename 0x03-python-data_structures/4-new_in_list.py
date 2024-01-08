48f4a50f3c4f3ef3470d85b1f4f06f0ba1d46901
sapiness <sapiness695@gmail.com>
2024-01-08 12:50:44 -0800

    element in a list at a specific position without modifying the original list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     temp = my_list[:]
     if idx < 0:
         return temp
-    if idx > len(my_list) - 1:
+    if idx > len(temp) - 1:
         return temp
     temp[idx] = element
     return temp
